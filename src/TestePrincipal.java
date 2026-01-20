74f94c92c0bdd2af69420a33dbda13ef870547bf
Samara Dias da Silva <marcoscz329@gmail.com>
2026-01-20 18:09:54 -0300

    Atualização do main

--- src/TestePrincipal.java
+++ src/TestePrincipal.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class TestePrincipal {
+    public static void main(String[] args) {
+        ArrayList<Produto>listaDeProdutos = new ArrayList<>();
+
+        Produto produto1 = new Produto("Garfo", 4.90,7);
+        Produto produto2 = new Produto("Panela", 50.90, 3);
+        Produto produto3 = new Produto("Copo de vidro", 5.60, 9);
+        listaDeProdutos.add(produto1);
+        listaDeProdutos.add(produto2);
+        listaDeProdutos.add(produto3);
+
+        System.out.println("O tamanho da lista: " + listaDeProdutos.size());
+        System.out.println("Primeira da lista: " + listaDeProdutos.get(0).getNome());
+
+        for (Produto produto: listaDeProdutos) {
+            System.out.println(produto);
         }
+
+        Produto produtoNovo = new Produto("Novo produto", 23.40, 5);
+        System.out.println(produtoNovo);
+
+        ProdutoPerecivel produtoPerecivel = new ProdutoPerecivel("Produto C", 12.75, 2, "2023-12-31");
+        System.out.println(produtoPerecivel);
+
+
     }
 }
